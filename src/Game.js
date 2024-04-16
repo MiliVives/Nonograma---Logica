81a0de8abc07c2cfc51ee9c9ff6efc952f58bf5d
MiliVives <milivives@gmail.com>
2024-04-16 14:23:06 -0300

    cositas

--- src/Game.js
+++ src/Game.js
@@ -3,6 +3,7 @@ import PengineClient from './PengineClient';
 import Board from './Board';
 
 let pengine;
+let content = 'X';
 
 function Game() {
 
@@ -37,9 +38,16 @@ function Game() {
     if (waiting) {
       return;
     }
+
+  let newContent;
+  if (grid[i][j] === '#') {
+    newContent = '_'; // If the square contains '#', set the new content to '_'
+  } else {
+    newContent = content; // Otherwise, use the current content
+  }
+
     // Build Prolog query to make a move and get the new satisfacion status of the relevant clues.    
     const squaresS = JSON.stringify(grid).replaceAll('"_"', '_'); // Remove quotes for variables. squares = [["X",_,_,_,_],["X",_,"X",_,_],["X",_,_,_,_],["#","#","#",_,_],[_,_,"#","#","#"]]
-    const content = '#'; // Content to put in the clicked square.
     const rowsCluesS = JSON.stringify(rowsClues);
     const colsCluesS = JSON.stringify(colsClues);
     const queryS = `put("${content}", [${i},${j}], ${rowsCluesS}, ${colsCluesS}, ${squaresS}, ResGrid, RowSat, ColSat)`; // queryS = put("#",[0,1],[], [],[["X",_,_,_,_],["X",_,"X",_,_],["X",_,_,_,_],["#","#","#",_,_],[_,_,"#","#","#"]], GrillaRes, FilaSat, ColSat)
@@ -51,6 +59,11 @@ function Game() {
       setWaiting(false);
     });
   }
+
+  function handleButtonClick(buttonId) {
+    console.log(`Button ${buttonId} clicked`);
+    content = buttonId === 'button1' ? 'X' : '#';
+  }  
 
   if (!grid) {
     return null;
@@ -64,6 +77,10 @@ function Game() {
         colsClues={colsClues}
         onClick={(i, j) => handleClick(i, j)}
       />
+      <div className="buttons">
+      <button className="button" onClick={() => handleButtonClick('button1')}>X</button>
+      <button className="button" onClick={() => handleButtonClick('button2')}>#</button>
+    </div>
     </div>
   );
 }
